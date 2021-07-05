3a6a0aea3dcde32c5dd93ea428653d2c18333a0d
정원영 <dnjsdud8185@naver.com>
2021-07-05 19:23:37 +0900

    Update plugin method

--- lib/flutter_piano_audio_detection.dart
+++ lib/flutter_piano_audio_detection.dart
@@ -6,6 +6,7 @@ import 'note_parser.dart';
 enum audioPermissionState { GRANTED, DENIED }
 enum tfLiteState { WAIT, PERMISSION_DENIED, ISLODING, SUCCESS, ERROR }
 enum recordingState { START, STOP, ERROR }
+final String logTag = "FLUTTER_PIANO_AUDIO_DETECTION_PLUGIN : ";
 
 class FlutterPianoAudioDetection {
   NoteParser noteParser = new NoteParser();
@@ -13,7 +14,6 @@ class FlutterPianoAudioDetection {
   recordingState _recordingState = recordingState.STOP;
   audioPermissionState _audioPermissionState = audioPermissionState.DENIED;
 
-  // Method & Event Channel
   static const MethodChannel _channel =
       const MethodChannel('flutter_piano_audio_detection');
 
@@ -46,26 +46,38 @@ class FlutterPianoAudioDetection {
       loadedModel
           ? _tfLiteState = tfLiteState.SUCCESS
           : _tfLiteState = tfLiteState.ERROR;
-      // .then((value) => _tfLiteState = tfLiteState.SUCCESS)
-      // .onError((error, stackTrace) => _tfLiteState = tfLiteState.ERROR);
     } else {
       _tfLiteState = tfLiteState.PERMISSION_DENIED;
     }
   }
 
   start() async {
-    await _channel.invokeMethod("start");
+    if (_tfLiteState == tfLiteState.SUCCESS) {
+      await _channel.invokeMethod("start");
+      _recordingState = recordingState.START;
+    } else {
+      print(logTag + "audio permission denied.");
+    }
   }
 
   stop() async {
     await _channel.invokeMethod("stop");
-    _channel.setMethodCallHandler((call) async {});
+    _recordingState = recordingState.STOP;
+  }
+
+  List<String> getNotes(List<dynamic> event) {
+    List<String> notes = [];
+    event.forEach((element) {
+      notes.add(getNoteName(element));
+    });
+    return notes;
   }
 
   String getNoteName(int midiNumber) {
-    return noteParser.getNoteName(midiNumber);
+    return noteParser.getNoteName(midiNumber - 20);
   }
 
   tfLiteState get getTfLiteState => _tfLiteState;
+  recordingState get getRecordingState => _recordingState;
   audioPermissionState get getAudioPermissionState => _audioPermissionState;
 }
